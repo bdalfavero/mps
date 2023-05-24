e2ec960b4ba7d63c5974f02b3b23d81158994091
Benjamin DalFavero <bdalfavero@gmail.com>
2023-05-24 15:53:36 -0700

    Shortened lines that were too long.

--- product_operators.py
+++ product_operators.py
@@ -19,9 +19,9 @@ def tensor_to_mpo(tensor):
     Converts the tensor representation of an operator into the equivalent MPO.
     The dimensions of the tensor should be ordered so that the "output state" dimensions
     come first, ordered from left to right, and then the "input states" come after, also
-    ordered from left to right. If the Hamiltonian was expressed in terms of outer-product projectors,
-    the first N dimensions of the tensor would correspond to the ket, and the last N dimensions to the bra
-    of the outer product.
+    ordered from left to right. If the Hamiltonian was expressed in terms of outer-product
+    projectors, the first N dimensions of the tensor would correspond to the ket, 
+    and the last N dimensions to the bra of the outer product.
 
     Arugmens:
     tensor: np.array, tensor representation of the operator.
@@ -40,7 +40,8 @@ def tensor_to_mpo(tensor):
     input_shape = old_shape[sites:]
     assert output_shape == input_shape, "Input and output dimensions must match."
     # Make a list of the new order for the axes.
-    axis_permutation = list(itertools.chain.from_iterable(zip(range(sites), range(sites, 2 * sites))))
+    axis_permutation = list(itertools.chain.from_iterable(zip(range(sites), 
+        range(sites, 2 * sites))))
     # Reshape the tensor to interleave the input and output dimensions
     new_tensor = np.transpose(tensor, axes=axis_permutation)
     # Start by reshaping the tensor into 1xN, becuase each iteration we will reshape it into
@@ -62,10 +63,12 @@ def tensor_to_mpo(tensor):
         if i == 0:
             # For the left tensor, the sequence of SVD's leaves us a shape (out_dim, in_dim), a_1.
             # We want a final shape out_dim, in_dim, a_1.
-            # NB: parentheses around indices mean they are grouped into the row or column of a matrix.
+            # NB: parentheses around indices mean they are grouped into the row or column 
+            # of a matrix.
             matrices[i] = matrices[i].reshape((out_dim, in_dim, matrices[i].shape[1]))
         elif i == sites - 1:
-            # For the rightmost tensor, the sequence of SVD's leaves a shape a_{L-1}, (out_dim, in_dim).
+            # For the rightmost tensor, the sequence of SVD's leaves a shape 
+            # a_{L-1}, (out_dim, in_dim).
             # The desired final shape is a_{L-1}, out_dim, in_dim.
             matrices[i] = matrices[i].reshape((matrices[i].shape[0], out_dim, in_dim))
         else:
@@ -73,7 +76,8 @@ def tensor_to_mpo(tensor):
             # After the sequence of SVD's, they have the shape (a_{i-1}, out_dim, in_dim), a_{i}.
             # We want a final shape of a_{i-1}, out_dim, in_dim, a_{i}.
             left_bond_dim = matrices[i].shape[0] // (out_dim * in_dim)
-            matrices[i] = matrices[i].reshape((left_bond_dim, out_dim, in_dim, matrices[i].shape[1]))
+            matrices[i] = matrices[i].reshape((left_bond_dim, 
+                out_dim, in_dim, matrices[i].shape[1]))
     # Build an MPO with these tensors.
     mpo = ProductOperator()
     mpo.tensors = matrices
